2596671adb7a4c436bc0b29478d1890d199bd68c
z <ahmedzubair216@gmail.com>
2018-01-31 21:09:55 +0530

    image opt plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,22 @@ module.exports = {
     title: `Md-ZubirAhmed's website`,
   },
   plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-plugin-sharp`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+              linkImagesToOriginal: true,
+              sizeByPixelDensity: true,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify`,
