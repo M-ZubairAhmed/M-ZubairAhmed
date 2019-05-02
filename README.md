9e5b0b170b66f468580507cb7271d371ceb4a2d5
m-zubairahmed <m-zubairahmed@protonmail.com>
2019-05-03 04:42:05 +0530

    netlify badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <h1 align="center">
   My webspace
 </h1>
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/72ba2e0a-1b75-474d-9ad4-585c847df8ff/deploy-status)](https://app.netlify.com/sites/mzubairahmed/deploys)
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,5 @@ import Jumbotron from '../components/jumbotron';
 export default () => (
   <Layout>
     <Jumbotron title="My Blog" description="Writing small blog posts, so i can share what i have experience and learned"/>
-    <hr />
   </Layout>
 )
